55cc579f6037000d632e87a22ae4a6bd4ff356e7
Jaewoong <dixk3458@naver.com>
2023-10-25 15:59:30 +0900

    edit

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { useAuthContext } from '../contexts/AuthContext';
 import { Navigate } from 'react-router-dom';
 
-export default function ProtectedRoute({ child, requireAdmin }) {
+export default function ProtectedRoute({ children, requireAdmin }) {
   const { user } = useAuthContext();
 
   if (!user || (requireAdmin && !user.isAdmin)) {
     return <Navigate to="/" replace={true} />;
   }
 
-  return child;
+  return children;
 }
